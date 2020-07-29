f610f6e3fb8fdc3ec824110828aec27bf60673ab
Thamaraikannan <thamarai.itps@gmail.com>
2020-07-29 19:59:26 +0530

    change in poly

--- poly.php
+++ poly.php
@@ -2,21 +2,24 @@
 class Shap  
 {  
 function draw(){}  
-}  
+}
+  
 class Circle extends Shap  
 {  
 function draw()  
 {  
 print "Circle has been drawn.</br>";  
 }  
-}  
+} 
+ 
 class Triangle extends Shap  
 {  
 function draw()  
 {  
 print "Triangle has been drawn.</br>";  
 }  
-}  
+} 
+ 
 class Ellipse extends Shap  
 {  
 function draw()  
@@ -24,7 +27,14 @@ function draw()
 print "Ellipse has been drawn.";  
 }  
 }  
-   
+
+class Rounds extends Shap  
+{  
+function draw()  
+{  
+print "Rounds has been drawn.";  
+}  
+}   
 $Val=array(2);  
   
 $Val[0]=new Circle();  
